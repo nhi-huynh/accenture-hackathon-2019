f1e18e5ce87b32d6e9b12db7c88988a042bd5c0d
Sam Breznikar <39972542+s-brez@users.noreply.github.com>
2019-11-30 15:05:20 +1100

    add loan constructor

--- loan.py
+++ loan.py
@@ -1,2 +1,27 @@
 class Loan:
-    """Loan class models the lending product utilised."""
+    """ Loan class models the lending product utilised."""
+
+    def __init__(self, api, logger, asset, deposit, duration, agent):
+        self.api = api
+        self.logger = logger
+
+        # Asset we are lending (string)
+        self.asset = asset
+
+        # intended size of the loan, in native denomination
+        self.total = deposit
+
+        # Current price of the asset in USD (one unit)
+        self.current_price = 0
+
+        # Current daily interest rate of the loan
+        self.current_interest_rate = 0
+
+        # Date of termination of the loan
+        self.close_date = self.close_date(duration)
+
+        # Loan agent runs in background in a new thread
+        self.loan_agent = agent
+
+        # Loan status - when loan terminated, this will be false.
+        self.active = True
