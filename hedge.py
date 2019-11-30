b90c7e5e0fccd47feaa5f612f1e808f11c34141e
Sam Breznikar <39972542+s-brez@users.noreply.github.com>
2019-11-30 16:33:45 +1100

    add hedge api calls

--- hedge.py
+++ hedge.py
@@ -5,7 +5,7 @@ import logging
 
 
 class Hedge:
-    """	Hedge class models the hedge taken to mitigate loan volatility."""
+    """	Hedge class models the hedge taken to mitigate loan volatility. """
 
     """ Industry-standard monthly futures contract codes. The month code
         signifies contract delivery month e.g AUDJPYZ19 would be a contract
@@ -39,3 +39,33 @@ class Hedge:
 
         # Hedge status - when hedge terminated, this will be false.
         self.active = True
+
+    def open_hedge(self):
+        """Open a hedge (short positon) with the specified instrument, default
+        is perpetual swap contact."""
+
+        order = api.Order.Order_new(
+            symbol=self.current_instrument,
+            orderQty=(self.value * -1)).result()
+
+        print("Hedge opened for " + self.value + " contracts.")
+        self.active = True
+
+        # Return transaction details json
+        return order
+
+    def terminate_hedge(self):
+        """Close the hedge postion and return confirmation dict containing
+        details of the closed positon. Hedge cLosure effective immediately."""
+
+        # Close all positions
+        result = api.Order.Order_new(
+            symbol=self.current_instrument,
+            ordType='Market',
+            execInst='Close').result()
+
+        print("Hedge closed for " + result[0]['orderQty'] + " contracts.")
+        self.active = False
+
+        # Return transaction details json
+        return result
