acedce249763d90f9dd0cbd9f56cd129a7ae2a95
Nhi Huynh <uyennhi.huynhluu@gmail.com>
2019-12-01 09:25:55 +1100

    Debugged user.py

--- user.py
+++ user.py
@@ -1,15 +1,15 @@
-
 class User:
     """User class models the customer (account holder)."""
 
-    def __init__(self, user_id, fname, lname, email, pwd):
+    def __init__(self, user_id, fname, lname, email, pwd, account=None):
         self.user_id = user_id
         self.first_name = fname
         self.last_name = lname
         self.email = email
         self.password_hash = pwd
-        self.account = None
+        self.account = account
 
     def init_account(self, account):
         """Create the account object. Account needs to be started with
         account.open_account() after creation to init the loan and hedge."""
+        self.account = account
