823acb1888f2170554c611f54a9462958e7e7168
Sam Breznikar <39972542+s-brez@users.noreply.github.com>
2019-11-30 14:46:52 +1100

    add hedge constructor

--- hedge.py
+++ hedge.py
@@ -1,11 +1,7 @@
-from swagger_spec_validator.common import SwaggerValidationWarning
-from polo_loan_agent import LoanAgent
-import warnings
-import datetime
+from time import time, sleep, strptime
 import requests
-import poloniex
-import bitmex
-import json
+import datetime
+import logging
 
 
 class Hedge:
@@ -18,3 +14,28 @@ class Hedge:
     FUTURES_MONTH_CODES = {
         'F': 'Jan', 'G': 'Feb', 'H': 'Mar', 'J': 'Apr', 'K': 'mar', 'M': 'Jun',
         'N': 'Jul', 'Q': 'Aug', 'U': 'Sep', 'V': 'Oct', 'X': 'Nov', 'Z': 'Dec'}
+
+	def __init__(self, logger, api, asset, value, duration):
+        self.logger = logger
+        self.api = api
+
+        # Asset the hedge is covering. For our example, Bitcoin
+        self.underlying_asset = asset
+
+        # USD value of the hedge. Must have exact 1:1 parity with loan size.
+        self.value = value
+
+        # Fixed termination date of the hedge (and loan).
+        self.close_date = self.close_date(duration)
+
+        # Use the perpetual swap contract as the default hedging instrument.
+        self.default_instrument = "XBTUSD"
+
+        # Instrument code currently in use, perpetual swap is default.
+        self.current_instrument = self.default_instrument
+
+        # Futures rollover date for instrument in use.
+        self.rollover_date = None
+
+        # Hedge status - when hedge terminated, this will be false.
+        self.active = True
