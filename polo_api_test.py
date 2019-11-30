4a9676550b3e7f21a8400007909e6625fa6325d7
Nhi Huynh <uyennhi.huynhluu@gmail.com>
2019-12-01 09:48:15 +1100

    Debugged polo_api_test.py

--- polo_api_test.py
+++ polo_api_test.py
@@ -1,9 +1,11 @@
 from time import time, sleep, strptime
 from polo_loan_agent import LoanAgent
-import poloniex
+import poloniex 
 import requests
 import datetime
 import logging
+from account import Account
+from user import User
 
 
 def init_logger():
@@ -31,11 +33,16 @@ logger = init_logger()
 LOAN_PROVIDER = "Poloniex"
 LOAN_ASSET = "USDC"
 MIN_LOAN = 50
+
 POLO_API = poloniex.Poloniex(
-    key='',
-    secret=""
-        ""
-        "")
+    key='HHDRON6N-H7C3LA42-AXV8EMJI-LPLUQD2Y',
+    secret="c05289ca176fc5981118756accc84172a513609df14b8fa887"
+        "cf6cf980292f724efbacd5fcd925f810cfaf71c0edfff31a4d0cfc"
+        "1e41ea23a1a3cad1706d5c44")
+
+# api_key='HHDRON6N-H7C3LA42-AXV8EMJI-LPLUQD2Y'
+# api_secret="c05289ca176fc5981118756accc84172a513609df14b8fa887cf6cf980292f724efbacd5fcd925f810cfaf71c0edfff31a4d0cfc1e41ea23a1a3cad1706d5c44"
+# POLO_API = Poloniex(api_key, api_secret)
 
 PUBLIC_COMMANDS = [
     'returnTicker',
@@ -87,10 +94,16 @@ PRIVATE_COMMANDS = [
 # while 1:
 #     sleep(10)
 
-rate = POLO_API.returnActiveLoans()['provided']
+# rate = POLO_API.returnActiveLoans()['provided']
 # rate = float(rate) * 100
-print(rate)
+# print(rate)
 
-# print(POLO_API.returnAvailableAccountBalances())
+print(POLO_API.returnAvailableAccountBalances())
 # print(POLO_API.returnOpenLoanOffers())
 # print(POLO_API.returnLoanOrders('USDC'))
+
+test_user = User("U003", "Radhika", "Zawar", "rad.za@gmail.com", "password")
+test_account = Account(logger, "A001")
+test_user.init_account(test_account)
+test_account.open_account()
+print(test_account.get_lending_stats())
