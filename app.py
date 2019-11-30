e8537b13850b6ab253362d88d974d1f0e856f38f
Nhi Huynh <uyennhi.huynhluu@gmail.com>
2019-11-30 14:18:15 +1100

    Add comments for the User endpoints

--- app.py
+++ app.py
@@ -22,9 +22,34 @@ loan_interest_controller = EntityController("Loan_interests", "timestamp", "acco
 # A controller to the funding rates of the account
 funding_rate_controller = EntityController("Funding_rates", "timestamp", "account_id")
 
+#------------------------ INDEX ENDPOINTS ------------------------------  
+
 @app.route('/')
 def index():
     return "API for the DeFiBank app"
+
+#------------------------ USERS ENDPOINTS ------------------------------        
+# Use this json to test the user endpoints using Postman
+#   {
+#         "user_id": "U001",
+#         "first_name": "Nhi",
+#         "last_name": "Huynh",
+#         "email": "nhi.huynh@gmail.com",
+#         "password_hash": "password", 
+#         "account_id": "A001"
+#     }
+
+# Test with a second user
+# {
+#     "user_id": "U002",
+#     "first_name": "Sam",
+#     "last_name": "Breznika",
+#     "email": "sam.brez@gmail.com",
+#     "password_hash": "password", 
+#     "account_id": "A002",
+#     "account_type": "standard_user"
+# }
+# 
 
 @app.route('/users', methods=["GET"])
 def get_users():
