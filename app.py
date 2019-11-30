c40f7f4be4bd022a30cd56dd39109ba989d4e5fa
Nhi Huynh <uyennhi.huynhluu@gmail.com>
2019-11-30 14:11:55 +1100

    Specified endpoints for Users

--- app.py
+++ app.py
@@ -21,3 +21,27 @@ loan_interest_controller = EntityController("Loan_interests", "timestamp", "acco
 
 # A controller to the funding rates of the account
 funding_rate_controller = EntityController("Funding_rates", "timestamp", "account_id")
+
+@app.route('/')
+def index():
+    return "API for the DeFiBank app"
+
+@app.route('/users', methods=["GET"])
+def get_users():
+    return jsonify(user_controller.get_entities())
+
+@app.route('/users/<user_id>', methods=["GET"])
+def get_user_by_id(user_id):
+    return jsonify(user_controller.get_entity(user_id))
+
+@app.route('/users', methods=["POST"])
+def create_user():
+    return jsonify(user_controller.create_entity(request.json))
+
+@app.route('/users/<user_id>', methods = ["PUT"])
+def update_user(user_id):
+    return jsonify(user_controller.update_entity(request.json))
+
+@app.route('/users/<user_id>', methods = ["DELETE"])
+def delete_user(user_id):
+    return jsonify(user_controller.delete_entity(user_id))
